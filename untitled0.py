ff7b6e296436ced1b3a2a336ebe373b3521d2c16
hwang105814 <hwang105814@gmail.com>
2022-12-06 14:39:46 +0900

    upgrade to save scores

--- untitled0.py
+++ untitled0.py
@@ -3,13 +3,20 @@ import csv
 import os
 currentpath = os.getcwd()
 print(currentpath)
-i=1
+i=0
 f = open('summay.tsv', 'r', encoding='cp949')
 rdr = csv.reader(f, delimiter='\t')
 r = list(rdr)
 
+f1 = open('save.tsv','w',encoding='cp949')
 
-while i <496:
+while i <495: #max 496
     rouge = Rouge()
     scores = rouge.get_scores(r[i][0], r[i][1])
-    print(scores)
+    #print(scores)
+
+    f1.write(str(i+1))
+    f1.write(":")
+    f1.writelines(str(scores))
+    f1.write("\n")
+    i+=1
