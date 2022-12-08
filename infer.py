f63bac837495295b8d48b09964fec855b8f5333a
SeongMun-Hwang <hwang105814@gmail.com>
2022-12-09 01:27:51 +0900

    Update infer.py

--- infer.py
+++ infer.py
@@ -1,7 +1,7 @@
 #import pytorch
 #import streamlit as st
 #from kobart import get_kobart_tokenizer
-#from transformers.models.bart import BartForConditionalGeneration
+from transformers.models.bart import BartForConditionalGeneration
 
 #@st.cache
 # def load_model():
