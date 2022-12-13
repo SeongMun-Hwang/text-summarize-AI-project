86fcd92612d42260c006e0c8dc52c8f5e82af032
hwang105814 <hwang105814@gmail.com>
2022-12-13 13:44:37 +0900

    delete unused file

--- calculate.py
+++ calculate.py
@@ -4,13 +4,13 @@ import os
 currentpath = os.getcwd()
 print(currentpath)
 i=0
-f = open('영어.tsv', 'r', encoding='cp949')
+f = open('test.tsv', 'r', encoding='cp949')
 rdr = csv.reader(f, delimiter='\t')
 r = list(rdr)
 
 f1 = open('save.tsv','w',encoding='cp949')
 
-while i <495: #max 496
+while i <501: #max 496
     rouge = Rouge()
     scores = rouge.get_scores(r[i][0], r[i][1])
     #print(scores)
--- excelrouge.py
+++ excelrouge.py
@@ -7,15 +7,15 @@ currentpath = os.getcwd()
 print(currentpath)
 i=1
 
-f = open('save.tsv', 'w', encoding='cp949') #저장할 파일 이름
-
-filename='summary.xlsx' #요약문 담긴 엑셀 이름
+f = open('save.tsv', 'wt', encoding='cp949') #저장할 파일 이름
+filename='realfinal.xlsx' #요약문 담긴 엑셀 이름
 df=pd.read_excel(filename,engine='openpyxl')
 
 original=df['original'] #[1행 1열 문자] -> 1행 2열부터 1차원 배열로 저장
 model=df['model'] #[행 2열 문자] ->  2행 2열부터 1차원 배열로 저장ㅍ
 
-while i <496: #max 496 //라우지 계산해서 save.tsv로 저장
+print(df)
+while i <495: #max 496 //라우지 계산해서 save.tsv로 저장
     rouge = Rouge()
     scores = rouge.get_scores(original[i], model[i])
     #print(scores)
